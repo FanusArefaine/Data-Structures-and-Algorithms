d4845cf6e824f1f1e2a0b5d69b564a0bf55700d5
Fanus Arefaine <fanus.gebrehiwet@gmail.com>
2022-07-10 18:59:33 -0700

    I wrote a comment to make Stack class more clear

--- Data Structures/Stack.py
+++ Data Structures/Stack.py
@@ -2,6 +2,9 @@
 
 class Stack:
 
+    """ This is Stack Data Structure with a couple of methods. 
+    """
+
     def __init__(self):
         self.stack = []
         self.size = 0
@@ -20,7 +23,7 @@ class Stack:
 
     # remove an element from the stack
     def pop(self):
-        
+
         # Checking if the list is empty
         if self.isEmpty():
             print("The stack is empty!!")
@@ -28,27 +31,27 @@ class Stack:
 
         self.stack.pop()
         self.size -= 1
-    
-    
+
     # A function that returns the top value of the stack
+
     def peek(self):
-        #checking if the list is empty
+        # checking if the list is empty
         if self.isEmpty():
             print("The list is empty!!")
             return
         return self.stack[-1]
 
     def printStack(self):
-        #checking if the list is empty
+        # checking if the list is empty
         if self.isEmpty():
             print("The list is empty!!")
             return
-        
+
         for i in range(1, len(self.stack)+1):
             print(self.stack[-i])
 
-        
-if __name__=='__main__':
+
+if __name__ == '__main__':
 
     my_stack = Stack()
 
@@ -56,21 +59,5 @@ if __name__=='__main__':
     my_stack.push(3)
     my_stack.push(2)
     my_stack.push(1)
-    print("The stack has",my_stack.size,"items.")
+    print("The stack has", my_stack.size, "items.")
     my_stack.printStack()
-
-
-
-    
-
-    
-
-
-
-
-    
-    
-
-
-
-        
